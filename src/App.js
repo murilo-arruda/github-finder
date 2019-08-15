b4d72991e9523b054635421f2247c962ea52f18a
Murilo <muriloarrudap@gmail.com>
2019-08-15 17:35:00 -0300

    initial files

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import './App.css';
+import Navbar from './components/layout/Navbar';
+import Users from './components/users/Users';
+import Search from './components/users/Search';
+
+import axios from 'axios';
+
+class App extends Component {
+  state = {
+    users: [],
+    loading: false
+  };
+
+  searchUsers = async text => {
+    this.setState({ loading: true });
+    const res = await axios.get(
+      `http://api.github.com/search/users?q=${text}&client_id=${
+        process.env.REACT_APP_GITHUB_CLIENT_ID
+      }&client_secret${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+    this.setState({ users: res.data.items, loading: false });
+  };
+
+  clearUsers = () => this.setState({ users: [], loading: false });
+  render() {
+    return (
+      <div className='App'>
+        <Navbar />
+        <div className='container'>
+          <Search
+            searchUsers={this.searchUsers}
+            clearUsers={this.clearUsers}
+            showClear={this.state.users.length > 0 ? true : false}
+          />
+          <Users loading={this.state.loading} users={this.state.users} />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/users/Search.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+
+const Search = ({ searchUsers, clearUsers, showClear }) => {
+  const [text, setText] = useState('');
+  const onChange = e => {
+    setText(e.target.value);
+  };
+  const onSubmit = e => {
+    e.preventDefault();
+    searchUsers(text);
+    setText('');
+  };
+  return (
+    <div>
+      <form onSubmit={onSubmit} className='form'>
+        <input
+          type='text'
+          name='text'
+          placeholder='Search Users...'
+          value={text}
+          onChange={onChange}
+        />
+        <input
+          type='submit'
+          value='Search'
+          className='btn btn-dark btn-block'
+        />
+      </form>
+      {showClear && (
+        <button className='btn btn-light btn-block' onClick={clearUsers}>
+          Clear
+        </button>
+      )}
+    </div>
+  );
+};
+Search.propTypes = {
+  searchUsers: PropTypes.func.isRequired,
+  clearUsers: PropTypes.func.isRequired,
+  showClear: PropTypes.bool.isRequired
+};
+export default Search;
